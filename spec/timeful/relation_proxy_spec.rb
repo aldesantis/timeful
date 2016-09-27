23cc4c50fda3b4166bded0db2261e794d94a8769
Alessandro Desantis <desa.alessandro@gmail.com>
2016-09-28 01:34:25 +0300

    Test RelationProxy

--- spec/timeful/relation_proxy_spec.rb
+++ spec/timeful/relation_proxy_spec.rb
@@ -1,12 +1,34 @@
 # frozen_string_literal: true
 RSpec.describe Timeful::RelationProxy do
+  subject { described_class.new(object) }
+
   describe '#find_each' do
     context 'when the object responds to #find_each' do
-      it 'calls #find_each on the object'
+      let(:object) do
+        Class.new do
+          def find_each
+            yield 1
+          end
+        end.new
+      end
+
+      it 'calls #find_each on the object' do
+        expect { |block| subject.find_each(&block) }.to yield_with_args(1)
+      end
     end
 
     context 'when the object does not respond to #find_each' do
-      it 'calls #each on the object'
+      let(:object) do
+        Class.new do
+          def each
+            yield 1
+          end
+        end.new
+      end
+
+      it 'calls #each on the object' do
+        expect { |block| subject.find_each(&block) }.to yield_with_args(1)
+      end
     end
   end
 end
